fb8d356a0c6be02e2bfbba6b68ba1a08df1778e7
Mohamed-Amine9 <hayouniamine01@gmail.com>
2022-10-20 12:02:08 +0100

    Start

--- backend/spring-boot-ecommerce/src/main/java/com/example/springbootecommerce/Repository/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/example/springbootecommerce/Repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.example.springbootecommerce.Repository;
 
 import com.example.springbootecommerce.Entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long> {
 }
